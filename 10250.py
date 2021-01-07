c1ff2d0ebd7271baa59048276a79dd8309318f48
malangcongdduck <cielciel107@gmail.com>
2021-01-07 21:35:03 +0900

    step by step

---
+++ 10250.py
@@ -0,0 +1,16 @@
+t=int(input())
+
+for i in range(t):
+    h,w,n=map(int,input().split())
+
+    if n%h==0:
+        h_n=h
+        w_n=n//h
+    else:
+        h_n=n%h
+        w_n=n//h+1
+
+    if 1<=w_n<=9:
+        print(str(h_n)+'0'+str(w_n))
+    else:
+        print(str(h_n)+str(w_n))
---
+++ 2775.py
@@ -0,0 +1,16 @@
+ans=[[0 for j in range(14)] for i in range(15)]
+for i in range(15):
+    ans[i][0]=1
+for j in range(14):
+    ans[0][j]=j+1
+for i in range(1,15):
+    for j in range(1,14):
+        ans[i][j]=ans[i][j-1]+ans[i-1][j]
+
+t=int(input())
+for i in range(t):
+    k=int(input())
+    n=int(input())
+    print(ans[k][n-1])
+
+
