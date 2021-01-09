75b092895ce323b59d7e5078efd1046c8d57b7b3
malangcongdduck <cielciel107@gmail.com>
2021-01-10 02:34:36 +0900

    Silver

---
+++ 1978.py
@@ -0,0 +1,20 @@
+n=int(input())
+s=list(map(int,input().split()))
+s.sort()
+
+result=0
+
+for i in s:
+    true_p=1
+    if i==1:
+        continue
+    elif i==2:
+        pass
+    else:
+        for j in range(2,i):
+            if i%j==0:
+                true_p=0
+
+    result+=true_p
+
+print(result)
