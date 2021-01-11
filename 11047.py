7f348905df4fd69ded5298216a39cc4874e9665d
malangcongdduck <cielciel107@gmail.com>
2021-01-11 22:58:08 +0900

    Silver (Greedy)

---
+++ 11047.py
@@ -0,0 +1,21 @@
+import sys
+n,k=map(int,sys.stdin.readline().split())
+coin=[]
+
+for i in range(n):
+    coin.append(int(sys.stdin.readline()))
+
+coin.reverse()
+cnt=0
+
+for i in coin:
+    if k==0:
+        break
+    
+    if i>k:
+        continue
+    else:
+        cnt+=k//i
+        k%=i
+
+print(cnt)
