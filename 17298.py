cd10a8ad9d0aa1da5dee03e63464102508c0d298
malangcongdduck <cielciel107@gmail.com>
2021-01-14 01:55:41 +0900

    Gold (Stack)

---
+++ 17298.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+
+n=int(sys.stdin.readline())
+seq_li=list(map(int,sys.stdin.readline().split()))
+q=deque()
+ans=deque()
+
+for i in range(n-1,-1,-1):
+    while q:
+        if q[-1]<=seq_li[i]:
+            q.pop()
+        else:
+            ans.appendleft(q[-1])
+            q.append(seq_li[i])
+            break
+
+    if len(q)==0:
+        ans.appendleft(-1)
+        q.append(seq_li[i])
+
+#print(q)
+for i in ans:
+    print(i,end=' ')
---
+++ 2493.py
@@ -0,0 +1,24 @@
+from collections import deque
+import sys
+
+n=int(sys.stdin.readline())
+tower=list(map(int,sys.stdin.readline().split()))
+q=deque()
+ans=[]
+
+for i in range(n):
+    while q:
+        if q[-1][1]<tower[i]:
+            q.pop()
+        else:
+            ans.append(q[-1][0])
+            q.append([i+1,tower[i]])
+            break
+           
+    
+    if len(q)==0:
+        ans.append(0)
+        q.append([i+1,tower[i]])
+       
+for i in ans:
+    print(i,end=' ')
