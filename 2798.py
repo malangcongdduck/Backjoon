4649495aca0ad24483e354abce252ee7946e4839
malangcongdduck <cielciel107@gmail.com>
2021-01-14 22:49:03 +0900

    Brute force

---
+++ 2798.py
@@ -0,0 +1,13 @@
+import sys
+
+n,m=map(int,sys.stdin.readline().split())
+card=list(map(int,sys.stdin.readline().split()))
+max=min(card)
+
+for i in range(n-2):
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
+            if card[i]+card[j]+card[k]<=m and card[i]+card[j]+card[k]>=max:
+                max=card[i]+card[j]+card[k]
+
+print(max)
