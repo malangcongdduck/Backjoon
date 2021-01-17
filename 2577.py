fe35a46f4427433c270a8a59c126a4e5cd22397b
malangcongdduck <76987629+malangcongdduck@users.noreply.github.com>
2021-01-17 23:36:04 +0900

    BOJ 2577 (Mathematics · String · Arithmetic, Bronze) Using Python

--- 2577.py
+++ 2577.py
@@ -5,4 +5,4 @@ c=int(input())
 result=list(str(a*b*c))
 
 for i in range(10):
-    print(result.count(str(i)))#i를 문자열로 바꿔서 result안에 i와 같은 것을 count
+    print(result.count(str(i)))#i를 문자열로 바꿔서 result안에 i와 같은 것을 count 
