07c339cccebac3b05eee4cdc4dab5ad73dd50242
malangcongdduck <cielciel107@gmail.com>
2021-01-23 14:47:19 +0900

    BOJ 11650 (Sorting, Silver) Using Python

---
+++ 11650.py
@@ -0,0 +1,12 @@
+import sys
+
+n=int(sys.stdin.readline())
+ans=[]
+for i in range(n):
+    x,y=map(int,sys.stdin.readline().split())
+    ans.append((x,y))
+
+ans.sort(key=lambda x:(x[0],x[1]))
+
+for i in ans:
+    print(i[0],i[1])
